032551135414848d4b659c6216256177e72da6bc
Ewerton Goncalves <ewerton.goncalves@vmetrix.com>
2024-09-28 16:30:56 -0300

    cloud icons

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
 export default defineConfig({
     plugins: [
         react(),
@@ -7,5 +8,5 @@ export default defineConfig({
     server: {
         open: true,
         host: true
-    },
+    }
 });
--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,6 @@ export default defineConfig({
   server: {
     open: true,
     host: true
-  },
+  }
 });
 
